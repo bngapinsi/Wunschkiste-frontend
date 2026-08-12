c1b6d10c30d1953e17a579e6514acf2013954217
bngapinsi <bngapinsi@gmail.com>
2026-08-12 15:15:16 +0200

    Update Wunsch Service

--- src/app/wunsch-list/wunsch-list.ts
+++ src/app/wunsch-list/wunsch-list.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { Wunsch } from '../wunsch';
+import { Wunsch, WunschItem } from '../wunsch';
 import { Auth } from '../auth';
 
 
@@ -11,9 +11,19 @@ import { Auth } from '../auth';
   templateUrl: './wunsch-list.html',
   styleUrl: './wunsch-list.css',
 })
-export class WunschList {
+export class WunschList implements OnInit{
+  wuensche: WunschItem[] = [];
+
   private wunschService = inject(Wunsch);
   private auth = inject(Auth);
 
   name = this.auth.getName();
+
+  ngOnInit(): void {
+    this.wunschService.getAll().subscribe({
+      next: (data) => this.wuensche = data,
+      error: (err) => console.error('Fehler beim Laden der Wünsche: ', err)
+    })
+  }
+  
 }
--- src/app/wunsch.ts
+++ src/app/wunsch.ts
@@ -1,6 +1,46 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface WunschItem{
+  id?:string;
+  titel:string;
+  kategorie:string;
+  preis:number;
+  link?:string;
+  bildUrl?:string;
+  notiz?:string;
+
+}
 
 @Injectable({
   providedIn: 'root',
 })
-export class Wunsch {}
+
+export class Wunsch {
+  private apiUrl = 'http://localhost:4200/wuensche';
+
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<WunschItem[]> {
+    return this.http.get<WunschItem[]>(this.apiUrl);
+  }
+
+  getOne(id: string): Observable<WunschItem> {
+    return this.http.get<WunschItem>(`${this.apiUrl}/${id}`)
+  }
+
+  create(wunsch: WunschItem): Observable<WunschItem> {
+    return this.http.post<WunschItem>(this.apiUrl, wunsch);
+  }
+
+  update(id: string, wunsch: Partial<WunschItem>): Observable<WunschItem> {
+    return this.http.patch<WunschItem>(`${this.apiUrl}/${id}`, wunsch);
+  }
+
+  delete(id: string): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+
+
+}
