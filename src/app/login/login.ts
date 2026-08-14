3910b6241b9f43535be2025eacd6a1bf5328a1d6
bngapinsi <bngapinsi@gmail.com>
2026-08-14 16:15:37 +0200

    Login Update

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -10,13 +10,45 @@ import { Router } from '@angular/router';
   styleUrl: './login.css',
 })
 export class Login {
+  //steuert welches Formular angezeigt wird
+  modus: 'anmelden' | 'registrieren' = 'anmelden';
+  //Formular für die Anmeldung
+  benutzername = '';
+  passwort = '';
+  //Formular für die Registstrierung
   vorname = '';
+  nachname = '';
+  neuerBenutzername = '';
+  neuesPasswort = '';
+
+  fehlerText = '';
 
   private auth = inject(Auth);
   private router = inject(Router);
 
-  registrieren(): void{
-    this.auth.setName(this.vorname);
+  modusWechseln(neuerModus: 'anmelden' | 'registrieren'): void {
+    this.modus = neuerModus;
+    this.fehlerText = '';
+  }
+
+  registrieren(): void {
+    this.auth.registrieren({
+      benutzername: this.neuerBenutzername,
+      vorname: this.vorname,
+      nachname: this.nachname,
+      passwort: this.neuesPasswort
+    });
     this.router.navigate(['/wuensche']);
   }
+
+  anmelden(): void {
+    const erfolgreich = this.auth.anmelden(this.benutzername, this.passwort);
+    if(erfolgreich) {
+      this.router.navigate(['/wuensche']);
+    }
+    else {
+      this.fehlerText = 'Benutzername oder Passwort falsch.';
+    }
+  }
+  
 }
