997cb9f1709e45751c31701022902859dfdbf427
bngapinsi <bngapinsi@gmail.com>
2026-08-17 22:33:13 +0200

    login korrektur

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Auth } from '../auth';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
