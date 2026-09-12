ee1a214fc86439b9a0368ec83490336a8c0e5cd1
bngapinsi <bngapinsi@gmail.com>
2026-09-12 23:01:49 +0200

    Änderungen

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -1,9 +1,10 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, signal } from '@angular/core';
+import { computed, Injectable, Signal, signal, WritableSignal } from '@angular/core';
 import { Observable } from 'rxjs';
 //Struktur eines registrierten Nutzers
 
 export interface Nutzer{
+  id?: string;
   benutzername: string;
   vorname: string;
   nachname: string;
@@ -19,10 +20,16 @@ export class Auth {
   constructor(private http: HttpClient) {}
 
   private currentName = signal<string>('');
+  private currentToken = signal<string>('');
+  private currentUsername = signal<string>('');
+  private currentRole = signal<string>('');
 
   getName() {
     return this.currentName;
   }
+
+  loggedIn = () => !!this.currentToken();
+  isAdmin = () => this.currentRole() === 'admin';
 
   //Registrierung: legt einen neuen Nutzer im Backend an
   registrieren(nutzer: Nutzer): Observable<Nutzer>{
